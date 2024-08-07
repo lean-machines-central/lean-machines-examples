e0babcf66064b759a78f2041303de9a798fed21c
Frederic Peschanski <frederic.peschanski@lip6.fr>
2024-08-07 16:41:36 +0200

    dependencies

---
+++ lakefile.lean
@@ -0,0 +1,16 @@
+import Lake
+open Lake DSL
+
+package «lean-machines-examples» where
+  -- add package configuration options here
+
+lean_lib «LeanMachinesExamples» where
+  -- add library configuration options here
+
+@[default_target]
+lean_exe «lean-machines-examples» where
+  root := `Main
+
+require «lean-machines» from git
+  "https://github.com/lean-machines-central/lean-machines.git"
+
