1a602720068078544a596fffb30efb4040bdd4ea
Frederic Peschanski <frederic.peschanski@lip6.fr>
2024-08-07 17:48:48 +0200

    buffer example

--- LeanMachinesExamples.lean
+++ LeanMachinesExamples.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `LeanMachinesExamples` library.
 -- Import modules here that should be built as part of the library.
-import «LeanMachinesExamples».Basic
+
+import LeanMachinesExamples.Buffer.Buffer2
--- Main.lean
+++ Main.lean
@@ -1,4 +1,4 @@
-import «LeanMachinesExamples»
+import LeanMachinesExamples
 
 def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+  IO.println "LeanMachines Examples"
